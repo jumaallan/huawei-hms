4871ef2ec4ba9ed4f00cf99565d5336c8a987283
Juma Allan <allanjuma@gmail.com>
2020-04-03 10:21:16 +0300

    Refactoring the dashboard

--- app/src/main/java/com/androidstudy/huaweihms/DashboardActivity.kt
+++ app/src/main/java/com/androidstudy/huaweihms/DashboardActivity.kt
@@ -1,12 +1,12 @@
 package com.androidstudy.huaweihms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
