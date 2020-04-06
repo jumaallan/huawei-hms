d4a8360e8746f0e3116da412819f892b50bd1c22
Juma Allan <allanjuma@gmail.com>
2020-04-06 16:56:57 +0300

    Setting up DI <> KOIN

---
+++ app/src/main/java/com/androidstudy/huaweihms/di/Modules.kt
@@ -0,0 +1,9 @@
+package com.androidstudy.huaweihms.di
+
+/**
+ * This is responsible for providing the following across the app
+ *      - Room DB and DAO's
+ *      - Settings
+ *      - Repositories
+ *      - ViewModels
+ * */
