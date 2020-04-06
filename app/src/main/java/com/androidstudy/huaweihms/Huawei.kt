8bc1d019348562e17e3da2db45e6aedb795cc43d
Juma Allan <allanjuma@gmail.com>
2020-04-06 17:14:41 +0300

    Setting up DI and Application class

--- app/src/main/java/com/androidstudy/huaweihms/Huawei.kt
+++ app/src/main/java/com/androidstudy/huaweihms/Huawei.kt
@@ -1,5 +1,43 @@
 package com.androidstudy.huaweihms
 
 import android.app.Application
+import androidx.annotation.Nullable
+import com.androidstudy.huaweihms.di.injectFeature
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import timber.log.Timber
 
-class Huawei : Application()
+open class Huawei : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+        initKoin()
+        injectFeature()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger()
+                androidContext(applicationContext)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+}
--- app/src/main/java/com/androidstudy/huaweihms/di/Modules.kt
+++ app/src/main/java/com/androidstudy/huaweihms/di/Modules.kt
@@ -1,5 +1,16 @@
 package com.androidstudy.huaweihms.di
 
+import android.content.Context
+import androidx.room.Room
+import com.androidstudy.huaweihms.data.Database
+import com.androidstudy.huaweihms.data.settings.Settings
+import com.androidstudy.huaweihms.repository.UserRepository
+import com.androidstudy.huaweihms.views.viewmodel.UserViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+
 /**
  * This is responsible for providing the following across the app
  *      - Room DB and DAO's
@@ -7,3 +18,52 @@ package com.androidstudy.huaweihms.di
  *      - Repositories
  *      - ViewModels
  * */
+
+fun injectFeature() = loadFeature
+
+private val loadFeature by lazy {
+
+    loadKoinModules(
+        listOf(
+            databaseModule,
+            daoModule,
+            repositoriesModule,
+            viewModelsModule,
+            settingsModule
+        )
+    )
+}
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            Database::class.java,
+            "huawei_db"
+        ).build()
+    }
+}
+
+val daoModule = module {
+    single { get<Database>().userDao() }
+}
+
+val repositoriesModule = module {
+    single { UserRepository(get()) }
+}
+
+val viewModelsModule = module {
+    viewModel { UserViewModel(get()) }
+}
+
+val settingsModule = module {
+    single {
+        androidContext().getSharedPreferences(
+            "huawei_settings",
+            Context.MODE_PRIVATE
+        )
+    }
+    single {
+        Settings(get())
+    }
+}
